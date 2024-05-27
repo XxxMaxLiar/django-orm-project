18104384705a23d060bcd104631f95b73f5389d6
XxxMaxLiar <xxxmaxliar@gmail.com>
2024-05-27 22:25:17 +0300

    pizdec vso proebal vse commits

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-passcard = Passcard.objects.all()
-print(passcard)
+    print('Количество пропусков:', Passcard.objects.count())   # noqa: T001
+    passcards = Passcard.objects.all()   # Импорт всех данных из БД
+    active_passcard = Passcard.objects.filter(is_active = True)   #Сохраняем в переменную только активных пользователей
+    print('Активных пропусков:', len(active_passcard))
